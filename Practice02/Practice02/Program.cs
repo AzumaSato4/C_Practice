5ae6f72e0d0e93e628706b5bd71bb84b0619872f
LEMON48\chinc <azumasato24@gmail.com>
2025-08-25 11:59:15 +0900

    条件分岐

--- Practice02/Practice02/Program.cs
+++ Practice02/Practice02/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//シンプルなif文
+
+using System.Runtime.CompilerServices;
+
+string money; //所持金
+Console.Write("所持金はいくらありますか？：");
+money = Console.ReadLine(); //ユーザーの入力待ち
+
+Console.WriteLine("所持金は" + money + "円ですね");
+
+
+//if文
+//型変換 文字列→整数
+int x;
+x = int.Parse(money);
+
+
+//xが1000以上なら
+if (x >= 1000)
+{
+    Console.WriteLine("まぐろ丼にします");
+}
+else if (x >= 500)
+{
+    Console.WriteLine("からあげ弁当にします");
+}
+else
+{
+    Console.WriteLine("そぼろ丼にします");
+}
+
+//switch文
+switch (x)
+{
+    case >= 1000: //xの中身が1000以上なら
+        Console.WriteLine("まぐろ丼にします");
+        break; //switch文を抜ける
+    case >= 500:
+        Console.WriteLine("からあげ弁当にします");
+        break;
+    default: //何にも該当しなかったら
+        Console.WriteLine("そぼろ丼にします");
+        break;
+}
--- Practice02/Practice02/obj/Debug/net8.0/Practice02.AssemblyInfo.cs
+++ Practice02/Practice02/obj/Debug/net8.0/Practice02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3456b67fda71307f353eb3489a3915b60defa50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
