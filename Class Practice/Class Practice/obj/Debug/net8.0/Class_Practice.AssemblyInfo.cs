e531c5b2259a6057752e804eadb34129a252fde3
LEMON48\chinc <azumasato24@gmail.com>
2025-08-27 10:50:56 +0900

    リスト

--- Class Practice/Class Practice/obj/Debug/net8.0/Class_Practice.AssemblyInfo.cs
+++ Class Practice/Class Practice/obj/Debug/net8.0/Class_Practice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Class_Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+293691d1d15094c799714bf829ef1b357b605ac2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+948f08ae3910dd26f94c303ef021e7b9fc65a7eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Class_Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Class_Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
