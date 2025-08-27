37bf7f9c210efe85f706cea3023508c6b151f128
LEMON48\chinc <azumasato24@gmail.com>
2025-08-27 14:50:31 +0900

    HelloFrom

---
+++ HelloForm/HelloForm/Form1.cs
@@ -0,0 +1,15 @@
+namespace HelloForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloButtonClicked(object sender, EventArgs e)
+        {
+            helloLable.Text = "Hello World";
+        }
+    }
+}
